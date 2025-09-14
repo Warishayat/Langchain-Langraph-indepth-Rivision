e4c4400970870407d2e33a9664a5e5dd9223db0e
waris hayat <warishayat666@gmail.com>
2025-09-14 20:46:47 +0500

    Retrivers

---
+++ Langchain/Indexes/Retriver/file1.py
@@ -0,0 +1,61 @@
+# there are mutiple type of retriver
+# 1: wikipedia artical
+# 2: vector store
+# 3: Archeive Retriver
+# Alg retriver alg alg treeky se search krte haen.
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import Chroma
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.retrievers import WikipediaRetriever
+import warnings
+warnings.filterwarnings('ignore')
+import os
+
+
+
+#1: Wikipedia retriver
+load_dotenv()
+retriver = WikipediaRetriever(top_k_results=3,lang='en')
+HUGGINGFACEHUB_ACCESS_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+embedding_mod = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+chat_model = ChatGoogleGenerativeAI(model="Gemini-2.5-flash",api_key=GOOGLE_API_KEY)
+
+
+
+# query = "history of england?"
+# res=retriver.invoke(query)
+# print(res)
+
+
+
+
+#load pdf
+loader = PyPDFLoader("purposal.pdf")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=300,chunk_overlap=30)
+docs = splitter.split_documents(docs)
+print(docs[0])
+
+vectorStore = Chroma(
+    embedding_function = embedding_mod,
+    collection_name = "pinecone_store",
+    persist_directory="Chroma_db"
+)
+
+vectorStore.add_documents(docs)
+
+search  =  vectorStore.as_retriever(kwargs={"k:2"})
+response=search.invoke("file structure?")
+print("Retriver")
+print("Answer is:")
+print(response)
+
+
+#lets make some advance search.
---
+++ Langchain/Indexes/Retriver/mqr.py
@@ -0,0 +1,58 @@
+# multi query retriver
+#lets make some advance search. mmr 
+# mmr jab search krta h tu wo try karta to bring something diffrent from each other instead
+# of getting the same result.
+
+# simple retriver kabhi kbhar same data de detay hn
+# jiski wjah se redundancy barh jati hae.
+# aur time aur cost ka waste hojta hae.
+
+# isi problem ko solve karta hae mmr 
+# yea krta kia h similar docuemnt uthata h query se
+# but wo document ik dusry se diffrent huty haen.
+# 
+
+#actually this is use when the query is ambigious and not understandle.
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import Chroma
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain.vectorstores import FAISS
+import warnings
+from langchain.retrievers import MultiQueryRetriever
+warnings.filterwarnings('ignore')
+import os
+
+
+
+#mmr retriver
+load_dotenv()
+HUGGINGFACEHUB_ACCESS_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+embedding_mod = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+chat_model = ChatGoogleGenerativeAI(model="Gemini-2.5-flash",api_key=GOOGLE_API_KEY)
+
+
+loader = PyPDFLoader("purposal.pdf")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=300,chunk_overlap=30)
+docs = splitter.split_documents(docs)
+
+retriver = FAISS.from_documents(
+    embedding=embedding_mod,
+    documents=docs
+)
+search = retriver.as_retriever(
+    kwargs={"k":3},
+    search_type="mmr",
+)
+
+MQR = MultiQueryRetriever.from_llm(llm=chat_model,retriever=retriver.as_retriever(kwargs={"k":5}))
+query = "about page"
+res = MQR.get_relevant_documents(query)
+print(res)
