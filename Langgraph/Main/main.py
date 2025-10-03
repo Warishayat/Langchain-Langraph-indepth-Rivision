0e231a0491009646638cdc13340f010e7e66ab44
waris hayat <warishayat666@gmail.com>
2025-10-03 20:59:09 +0500

    Chatbot ui + streaming

---
+++ Langgraph/Main/main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
+from dotenv import load_dotenv
+from pipeline import workflow
+import warnings
+warnings.filterwarnings('ignore')
+load_dotenv()
+
+
+#streaming in chatbot.
+Config = {'configurable' : {'thread_id':'1'}}
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+st.subheader("Chatbot with streming + langgraph")
+
+for msg in st.session_state['message_history']:
+    with st.chat_message(msg['role']):
+        st.text(msg['content'])
+
+user_input = st.chat_input("Type here")
+if user_input:
+
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk,meta_data in workflow.stream(
+                {'messages':[HumanMessage(content=user_input)]},
+                config  = Config,
+                stream_mode = 'messages'
+            )
+    )
+    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
+        
+   
---
+++ Langgraph/Main/pipeline.py
@@ -0,0 +1,50 @@
+import os 
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import BaseModel,Field
+from typing import Optional,Literal,Annotated
+from typing import TypedDict
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+from langgraph.graph import StateGraph,START,END
+from langchain_core.messages import AIMessage
+from langchain_core.messages import HumanMessage,AIMessage
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+Model = ChatGoogleGenerativeAI(model='gemini-2.5-flash',api_key=GOOGLE_API_KEY,temperature=.5)
+
+checkpointer = MemorySaver()
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage],add_messages]
+
+def chatbot(state:ChatState):
+    response = Model.invoke(state['messages']).content
+    message = [AIMessage(content=response)]
+    return{'messages':message}
+
+graph = StateGraph(ChatState)
+graph.add_node("chatbot",chatbot)
+graph.add_edge(START,'chatbot')
+graph.add_edge('chatbot',END)
+
+workflow = graph.compile(checkpointer=checkpointer)
+
+# initial_state = {'messages':[HumanMessage(content="how to apply german study visa and which universistat are best for llm reserach?")]}
+
+# thread_id = '1'
+# while True:
+#     message = input("User: ")
+#     print("User:",message)
+#     config = {"configurable": {"thread_id": thread_id}}
+#     if message.strip().lower() in ['exit','quit','bye']:
+#         break
+    
+#     response = workflow.invoke({'messages':[HumanMessage(content=message)]},config=config)
+#     print("AI :",response['messages'][-1].content)
