f49cc1aed16796d5c36f97c83e24b6695f86ec6c
waris hayat <warishayat666@gmail.com>
2025-10-04 12:46:19 +0500

    database integration + resume_chat

---
+++ Langgraph/Main/chat_database.py
@@ -0,0 +1,112 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from dotenv import load_dotenv
+from pipeline import workflow,get_all_threads
+import uuid
+import warnings
+
+
+warnings.filterwarnings('ignore')
+load_dotenv()
+
+
+def get_thread_id():
+    return str(uuid.uuid4())
+
+
+# *********************** Session State ***************************
+if "conversations" not in st.session_state:
+    st.session_state["conversations"] = {} 
+
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"] = get_thread_id()
+
+if "chat_threads" not in st.session_state:
+    st.session_state["chat_threads"] = get_all_threads()
+
+
+# ****************** functions ***********************************
+
+def new_chat_window():
+    thread_id = get_thread_id()
+    st.session_state["thread_id"] = thread_id
+    st.session_state["conversations"][thread_id] = []
+    add_thread(thread_id)
+    st.rerun()
+
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state["chat_threads"]:
+        st.session_state["chat_threads"].append(thread_id)
+    if thread_id not in st.session_state["conversations"]:
+        st.session_state["conversations"][thread_id] = []
+
+
+def get_conversation(thread_id):
+    state = workflow.get_state(config={"configurable": {"thread_id": thread_id}})
+    values = state.values
+    return values.get("messages", [])
+
+
+# *********************** Message History **************************
+current_thread = st.session_state["thread_id"]
+message_history = st.session_state["conversations"].get(current_thread, [])
+
+for msg in message_history:
+    with st.chat_message(msg["role"]):
+        st.text(msg["content"])
+
+
+# *********************** Chatbot ************************************
+add_thread(st.session_state["thread_id"])
+st.sidebar.title("Langgraph - Chatbot")
+
+if st.sidebar.button("New Chat"):
+    new_chat_window()
+
+st.sidebar.header("My Conversations")
+for thread_id in st.session_state["chat_threads"][::-1]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state["thread_id"] = thread_id
+        # Load conversation from workflow state (if exists)
+        message = get_conversation(thread_id)
+
+        msg_list = []
+        for m in message:
+            if isinstance(m, HumanMessage):
+                role = "user"
+            else:
+                role = "assistant"
+            msg_list.append({"role": role, "content": m.content})
+
+        st.session_state["conversations"][thread_id] = msg_list
+        st.rerun()  # refresh UI with correct history
+
+
+# *********************** Input & Streaming **************************
+user_input = st.chat_input("Type here")
+Config = {"configurable": {"thread_id": st.session_state["thread_id"]}}
+
+if user_input:
+    current_thread = st.session_state["thread_id"]
+
+    st.session_state["conversations"][current_thread].append(
+        {"role": "user", "content": user_input}
+    )
+
+    with st.chat_message("user"):
+        st.text(user_input)
+
+    with st.chat_message("assistant"):
+        ai_message = st.write_stream(
+            message_chunk.content
+            for message_chunk, meta_data in workflow.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=Config,
+                stream_mode="messages",
+            )
+        )
+
+    st.session_state["conversations"][current_thread].append(
+        {"role": "assistant", "content": ai_message}
+    )
--- Langgraph/Main/pipeline.py
+++ Langgraph/Main/pipeline.py
@@ -4,7 +4,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel,Field
 from typing import Optional,Literal,Annotated
 from typing import TypedDict
+import langgraph
 from langgraph.checkpoint.memory import MemorySaver
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage
 from langgraph.graph import StateGraph,START,END
@@ -13,13 +16,16 @@ from langchain_core.messages import HumanMessage,AIMessage
 
 load_dotenv()
 
+conn = sqlite3.connect(database='cahtbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
 Model = ChatGoogleGenerativeAI(model='gemini-2.5-flash',api_key=GOOGLE_API_KEY,temperature=.5)
 
-checkpointer = MemorySaver()
+# checkpointer = MemorySaver()
 
 class ChatState(TypedDict):
     messages : Annotated[list[BaseMessage],add_messages]
@@ -36,18 +42,8 @@ graph.add_edge('chatbot',END)
 
 workflow = graph.compile(checkpointer=checkpointer)
 
-initial_state = {'messages':[HumanMessage(content="how to apply german study visa and which universistat are best for llm reserach?")]}
-
-thread_id = '1'
-config = {"configurable": {"thread_id": thread_id}}
-response=workflow.invoke({'messages':[HumanMessage(content="hello how are you?")]},config=config)
-print(workflow.get_state(config=config).values['messages'])
-# while True:
-#     message = input("User: ")
-#     print("User:",message)
-
-#     if message.strip().lower() in ['exit','quit','bye']:
-#         break
-    
-#     response = workflow.invoke({'messages':[HumanMessage(content=message)]},config=config)
-#     print("AI :",response['messages'][-1].content)
+def get_all_threads():
+    all_threads = set()
+    for cp in checkpointer.list(None):
+        all_threads.add(cp.config['configurable']['thread_id'])
+    return list(all_threads)
